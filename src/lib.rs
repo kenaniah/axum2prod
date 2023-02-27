081eb1f6528181096125a57f063b5356108a42be
Kenaniah Cerny <kenaniah@gmail.com>
2023-02-27 15:53:07 -0800

    subscribe endpoint

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
+use axum::Form;
 use axum::{
     http::StatusCode,
-    routing::{get, IntoMakeService},
+    response::IntoResponse,
+    routing::{get, post, IntoMakeService},
     Router, Server,
 };
 use hyper::{self, server::conn::AddrIncoming};
@@ -12,8 +14,19 @@ pub fn run(
     // build our application with a single route
     let app = Router::new()
         .route("/", get(|| async { "Hello, World!" }))
-        .route("/health_check", get(|| async { StatusCode::OK }));
+        .route("/health_check", get(|| async { StatusCode::OK }))
+        .route("/subscriptions", post(subscribe));
 
     // run it
     Ok(axum::Server::from_tcp(listener)?.serve(app.into_make_service()))
+}
+
+#[derive(serde::Deserialize)]
+struct FormData {
+    name: String,
+    email: String,
+}
+
+async fn subscribe(Form(_data): Form<FormData>) -> impl IntoResponse {
+    StatusCode::CREATED
 }
