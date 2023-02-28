ff8e4397fc38715e0d82be9c84362f9b7c11f4ae
Kenaniah Cerny <kenaniah@gmail.com>
2023-02-27 22:31:03 -0800

    time

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,6 @@ async fn main() -> hyper::Result<()> {
     let config = get_config();
     let address = format!("127.0.0.1:{}", config.port);
     //.context("Failed to connect to Postgres.")?;
-    let listener = TcpListener::bind(&address).expect(&format!("Failed to bind to {}", &address));
+    let listener = TcpListener::bind(&address).unwrap_or_else(|_| panic!("Failed to bind to {}", &address));
     run(listener, config.db().await)?.await
 }
