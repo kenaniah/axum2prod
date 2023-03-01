1722b4e6d2384826d859a6e5ba87bd055de46eba
Kenaniah Cerny <kenaniah@gmail.com>
2023-03-01 00:35:50 -0800

    cleanup

--- crates/axum2prod/tests/health_check.rs
+++ crates/axum2prod/tests/health_check.rs
@@ -3,10 +3,15 @@ use std::{future::Future, net::TcpListener, pin::Pin, time::Duration};
 use axum2prod::configuration;
 use sqlx::PgPool;
 
+struct TestContext {
+    pub address: String,
+    pub db: PgPool,
+}
+
 /// Wrapper for tests to ensure each is run in an isolated environment
 async fn run_test<T>(test: T)
 where
-    T: FnOnce(String, PgPool) -> Pin<Box<dyn Future<Output = ()> + Send>> + std::panic::UnwindSafe,
+    T: FnOnce(TestContext) -> Pin<Box<dyn Future<Output = ()> + Send>> + std::panic::UnwindSafe,
 {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
@@ -20,7 +25,10 @@ where
     let handle = tokio::spawn(server);
 
     // Run the test
-    let task = tokio::spawn(test(address, test_db.pool.clone()));
+    let task = tokio::spawn(test(TestContext {
+        address,
+        db: test_db.pool.clone(),
+    }));
     let result = task.await;
 
     // Stop the server
@@ -40,11 +48,11 @@ where
 
 #[tokio::test]
 async fn health_check_works() {
-    run_test(|address, _db| {
+    run_test(|ctx| {
         Box::pin(async move {
             let client = reqwest::Client::new();
             let response = client
-                .get(&format!("{}/health_check", &address))
+                .get(&format!("{}/health_check", &ctx.address))
                 .send()
                 .await
                 .expect("Failed to execute request.");
@@ -57,7 +65,7 @@ async fn health_check_works() {
 
 #[tokio::test]
 async fn subscribe_returns_a_201_for_valid_form_data() {
-    run_test(|address, db| {
+    run_test(|ctx| {
         Box::pin(async move {
             // Arrange
             let client = reqwest::Client::new();
@@ -65,7 +73,7 @@ async fn subscribe_returns_a_201_for_valid_form_data() {
             // Act
             let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
             let response = client
-                .post(&format!("{}/subscriptions", &address))
+                .post(&format!("{}/subscriptions", &ctx.address))
                 .header("Content-Type", "application/x-www-form-urlencoded")
                 .body(body)
                 .send()
@@ -76,7 +84,7 @@ async fn subscribe_returns_a_201_for_valid_form_data() {
             assert_eq!(201, response.status().as_u16());
 
             let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
-                .fetch_one(&db)
+                .fetch_one(&ctx.db)
                 .await
                 .expect("Failed to fetch saved subscription.");
             assert_eq!(saved.email, "ursula_le_guin@gmail.com");
@@ -88,7 +96,7 @@ async fn subscribe_returns_a_201_for_valid_form_data() {
 
 #[tokio::test]
 async fn subscribe_returns_a_422_when_data_is_missing() {
-    run_test(|address, _db| {
+    run_test(|ctx| {
         Box::pin(async move {
             // Arrange
             let client = reqwest::Client::new();
@@ -101,7 +109,7 @@ async fn subscribe_returns_a_422_when_data_is_missing() {
             for (invalid_body, error_message) in test_cases {
                 // Act
                 let response = client
-                    .post(&format!("{}/subscriptions", &address))
+                    .post(&format!("{}/subscriptions", &ctx.address))
                     .header("Content-Type", "application/x-www-form-urlencoded")
                     .body(invalid_body)
                     .send()
