76cd7e6223b3bac5e3bc9d86c46dc7e7f260ce69
Kenaniah Cerny <kenaniah@gmail.com>
2023-03-01 23:40:30 -0800

    cleanup

--- crates/axum2prod/src/main.rs
+++ crates/axum2prod/src/main.rs
@@ -1,11 +1,10 @@
 use axum2prod::{get_config, run};
-use env_logger::Env;
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> hyper::Result<()> {
     // Initialize the logger
-    env_logger::Builder::from_env(Env::default().default_filter_or("debug")).init();
+    env_logger::init();
 
     // Configure the server
     let config = get_config();
